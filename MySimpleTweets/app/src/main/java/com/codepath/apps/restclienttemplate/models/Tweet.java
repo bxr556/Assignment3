fe7ff3f84141297572916e01d255a126813a8c6a
pan maker <pan.maker.1870@gmail.com>
2017-09-28 23:09:54 -0700

    initial checkin

---
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,29 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by qunli on 9/26/17.
+ */
+
+public class Tweet {
+
+    //list of attributes
+    public String body;
+    public long uid;
+
+    public User user;
+
+    public String createdAt;
+
+    //deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+}
